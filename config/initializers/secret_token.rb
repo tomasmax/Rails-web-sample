dc318a8f333915d9ad9290be9bb51ad8333db594
unknown <Tomy@.(none)>
2011-11-11 00:10:17 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialTour::Application.config.secret_token = '9cd137b024de04b902f7515f9905626b9505b938aabc40a59665b4e6cc22eb40fc08e29d807bfaceee2567043be28617262e5bbdf7728b6066a5e2035318fce2'
